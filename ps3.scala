49de7f9aecd9001b36e9bc0ab68a2eeca989b0b3
Ragib Morshed <rmorshed@adconion.com>
2015-08-08 14:14:43 -0700

    saving

--- ps3.scala
+++ ps3.scala
@@ -10,9 +10,14 @@ object ScalaSchool {
         lst.flatMap(e => lst.map((e, _))) 
     }
 
+    def tuplePair(lst: List[(Int, Int)]): List[((Int, Int),(Int, Int))] = {
+        lst.flatMap(e => lst.map((e, _)))
+    }
+
     def main(args: Array[String]): Unit = {
         val numbers = List(1,2,3,4)
-    
+        val tupleList = List((1,0),(0,1),(1,1),(0,0))
+
         println("-fib-")
         fib.take(10).foreach(e => println(e))    
     
@@ -20,6 +25,11 @@ object ScalaSchool {
         println("numbers " + numbers)
         val tuples = pairs(numbers)
         println(tuples)
+    
+        println("-tuple pairs-")
+        println("tuple list " + tupleList)
+        val tuplesPair = tuplePair(tupleList)
+        println(tuplesPair)
     }
 
 }
