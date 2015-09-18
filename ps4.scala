5243750fed7c7600f9c217ab7817dd270296cd13
Ragib Morshed <rmorshed@adconion.com>
2015-09-18 11:16:56 -0700

    addad q2 to ps4

--- ps4.scala
+++ ps4.scala
@@ -4,9 +4,25 @@ object ScalaSchool {
         0  #:: fib.scanLeft(1){(m,n) => m + n}
     }
 
+    def isPalindrome(text: String): Boolean = {
+        val perms = text.permutations.toArray
+        val palindromes = perms.filter(e => (e == e.reverse)).length
+        palindromes > 0
+    }
+
     def main(args: Array[String]): Unit = {
         println("-Q1-")
         var sum = fib.take(50).filter(e => (e  % 2 == 0 && e < 4000000)).foldLeft(0)(_ + _)
         println(sum)
+
+        println("-Q2-")
+        var text = "civic"
+        println(text + " -> " + isPalindrome(text))
+        text = "ivicc"
+        println(text + " -> " + isPalindrome(text))
+        text = "civil"
+        println(text + " -> " + isPalindrome(text))
+        text = "livci"
+        println(text + " -> " + isPalindrome(text))
     }
 }
